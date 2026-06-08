56abea4d50989e307414fe3239196c38754d8cab
BLUE000 <take.1229.blue@gmail.com>
2026-06-08 10:52:49 +0900

    feat: design and wire up basic UI layout for Viewer, Analysis, and Settings tabs

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,11 +4,33 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , m_chatModel(new QStandardItemModel(0, 2, this))
 {
     ui->setupUi(this);
+
+    // ビューワタブのテーブル設定
+    m_chatModel->setHeaderData(0, Qt::Horizontal, "Username");
+    m_chatModel->setHeaderData(1, Qt::Horizontal, "Message");
+    ui->tableViewComments->setModel(m_chatModel);
+    ui->tableViewComments->setColumnWidth(0, 150);
+    ui->tableViewComments->horizontalHeader()->setStretchLastSection(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::addCommentToView(const QString& username, const QString& message) {
+    int newRow = m_chatModel->rowCount();
+    m_chatModel->insertRow(newRow);
+    m_chatModel->setData(m_chatModel->index(newRow, 0), username);
+    m_chatModel->setData(m_chatModel->index(newRow, 1), message);
+    
+    // 最新コメントへ自動スクロール
+    ui->tableViewComments->scrollToBottom();
+}
+
+void MainWindow::on_btnStartAuth_clicked() {
+    emit authRequested();
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -1,7 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#pragma once
 #include <QMainWindow>
+#include <QStandardItemModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    // UIのテーブルにコメントを追加する
+    void addCommentToView(const QString& username, const QString& message);
+
+signals:
+    // 設定タブで認証ボタンが押されたことを通知
+    void authRequested();
+
+private slots:
+    void on_btnStartAuth_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QStandardItemModel* m_chatModel;
 };
 #endif // MAINWINDOW_H
